6cdeee743a4438ef26360971885c87bafc2429ba
Michael Buraimoh <buraimohkolapo@gmail.com>
2023-06-12 04:08:31 -0700

    Python Data Structure

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return 'None'
+    else:
+        max_num = my_list[0]
+        for i in my_list:
+            if i > max_num:
+                max_num = i
+            else:
+                continue
+        return max_num
