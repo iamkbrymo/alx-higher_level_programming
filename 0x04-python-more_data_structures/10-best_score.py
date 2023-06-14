3c930d6ca966550ab0ddbfbf8bdb2bd446b8fd63
Michael Buraimoh <buraimohkolapo@gmail.com>
2023-06-14 08:01:37 -0700

    Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    score = 0
-    for k, v in a_dictionary.item():
-        if v > score:
-            score = v
-    return score
+    if a_dictionary:
+        my_list = list(a_dictionary.keys())
+        score = 0
+        leader = ""
+        for i in my_list:
+            if a_dictionary[i] > score:
+                score = a_dictionary[i]
+                leader = i
+        return leader
